faf0dd13a4e5a73fe5192ddf5e2afe5a7021d2a3
Hknots <hknots99@gmail.com>
2025-03-18 15:01:03 +0100

    build: upgrade java version to 21 & lombok version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 group = "no.fintlabs"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 repositories {
     maven("https://repo.fintlabs.no/releases")
     mavenCentral()
@@ -15,8 +20,8 @@ dependencies {
     implementation("no.fintlabs:fint-model-core:0.5.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
-    compileOnly("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -24,4 +29,24 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://repo.fintlabs.no/releases")
+            credentials {
+                username = System.getenv("REPOSILITE_USERNAME")
+                password = System.getenv("REPOSILITE_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
 }
